affae230324e3e475dd5fc4565d04d28a17d8d78
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-04-07 07:49:21 -0700

    add new category to categories page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -6,6 +6,20 @@ class Admin::CategoriesController < ApplicationController
     @categories = Category.order(id: :desc).all
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
   private
 
   def category_params
