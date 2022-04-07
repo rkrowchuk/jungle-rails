c4cdf4469c027c187759b2fea08b130b3a553fe1
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-04-07 12:41:18 -0700

    add new view to users, add form, update controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :firstname,
+      :lastname,
+      :email,
+      :password
+    )
+  end
+
 end
