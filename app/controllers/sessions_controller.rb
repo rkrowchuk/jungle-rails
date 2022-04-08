8747720eafcecdd615cdc5d709bc20c6da676a9c
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-04-07 18:16:01 -0700

    fix bug for displaying logged in user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
-  
+
   def new
   end
 
   def create
-    @user = User.find_by_email(params[:email])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
       redirect_to '/'
     else
       redirect_to '/login'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to :root
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
     else 
-      redirect_to [:users, :new] 
+      redirect_to '/signup'
     end
   end
 
