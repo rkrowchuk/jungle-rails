db044910ea9939d79d200fe76c16d29dc3d37d54
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-04-11 14:25:11 -0700

    generate product_spec

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+  
+    context 'when creating a new product' do
+      it 'has a name'
+
+  
+  end
+end
