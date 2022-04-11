0ea3c97c45e147c1078d025dc6de81dfe0bd9497
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-04-11 15:46:48 -0700

    add first validation to product_spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,11 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  describe 'Validations' do
-  
-    context 'when creating a new product' do
-      it 'has a name'
+  category = Category.new(name: "category")
+  subject {
+    described_class.new(name: "Name", price: 1000, quantity: 3, category: category)
+  }
 
-  
+  describe 'Validations' do
+      
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end  
   end
+
 end
