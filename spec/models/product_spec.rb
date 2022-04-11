a611862a5d2027df67675c84e456bd472aadb02f
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-04-11 16:38:10 -0700

    add validation tests for name, price, quantity and category

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,12 +4,21 @@ RSpec.describe Product, type: :model do
   category = Category.new(name: "category")
   subject {
     described_class.new(name: "Name", price: 1000, quantity: 3, category: category)
-  }
+  } 
 
   describe 'Validations' do
       
-    it 'is valid with valid attributes' do
-      expect(subject).to be_valid
+    it 'is valid with a name' do
+      expect(subject.name).to be_present
+    end  
+    it 'is valid with a price' do
+      expect(subject.price).to be_present
+    end 
+    it 'is valid with a quantity' do
+      expect(subject.quantity).to be_present
+    end 
+    it 'is valid with a category' do
+      expect(subject.category).to be_present
     end  
   end
 
