c106e700b9a8d719e360081722efa4f52dfdecd7
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-04-11 19:02:48 -0700

    add validations fornew user: first name, last name, password, email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,26 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  password_match = User.new(firstname: "First", lastname: "Last", email: "test@test.com", password: "password", password_confirmation: "password")
-  password_match.save
-  password_no_match = User.new(firstname: "First", lastname: "Last", email: "test@test.com", password: "password", password_confirmation: "wrong")
-  email_check = User.new(firstname: "First", lastname: "Last", email: "TEST@test.com", password: "password", password_confirmation: "password")
-  
+  subject {
+    described_class.new(firstname: "First", lastname: "Last", email: "test@test.com", password: "password", password_confirmation: "password")
+  }
 
   describe 'Validations' do
-    it 'has a password and password confirmation' do
-      expect(password_match).to be_present
+    it 'has an email' do
+      expect(subject.email).to be_present 
+    end
+    it 'has a first name' do
+      expect(subject.firstname).to be_present 
+    end
+    it 'has a last name' do
+      expect(subject.lastname).to be_present 
+    end
+    it 'has a password' do
+      expect(subject.password).to be_present
+    end
+    it 'has a password confirmation' do
+      expect(subject.password_confirmation).to be_present
     end
     it 'has a matching password and password confirmation' do
-      expect(password_match.password_confirmation).to match password_match.password
+      expect(subject.password_confirmation).to match subject.password
     end
     it 'is not valid when password and password confirmation do not match' do
-      expect(password_no_match.password_confirmation).not_to match password_no_match.password
+      subject.password_confirmation = "wrong"
+      expect(subject.password_confirmation).not_to match subject.password
     end
-    it 'has a unique email' do
-      expect()
-    end
-    it 'has an email, first name and last name' do
+    it 'it is not valid without a unique email' do
+      new_subject = User.create(firstname: "New", lastname: "User", email: "test@test.com", password: "password2", password_confirmation: "password2")
+      expect(subject).to_not be_valid
     end
   end
 end
