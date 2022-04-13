2942ce33d966d0a01c5c27ba04274a8b5f2d5a32
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-04-12 17:23:28 -0700

    implement test for visiting product details view

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor clicks into Product Details", type: :feature, js: true do
+  
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+  scenario "They see all products" do
+    visit root_path
+
+    first("article.product").find_link("Details").trigger("click")
+    expect(page).to have_css 'section.products-show' 
+  end
+end
